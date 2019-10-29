3708a6aad5a95c38d831943d5c5cd91ab7e21fcf
Dylan Barber <DylanBarber1@Outlook.com>
2019-10-29 09:38:47 -0400

    Fix comment format

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,22 @@
-//Repeat boolean for checking if a new color should be generated
+ // Repeat boolean for checking if a new color should be generated
 let repeat = true;
 
-//Create array for history tracking
+ // Create array for history tracking
 let historyArray = [];
 
-//Create array for favorites
+ // Create array for favorites
 let favoritesArray = [];
 
-//prevItem for highlighting rows in historyBox when clicked
+ // prevItem for highlighting rows in historyBox when clicked
 let prevItem = null;
 
-//For the current highlighted intem in historyBox
+ // For the current highlighted intem in historyBox
 let currentItem = null;
 
-//For storage of the current color
+ // For storage of the current color
 let currentColor = {};
 
-//For checking if DOM elements should be displayed or not. Displays DOM elements when first color is generated
+ // For checking if DOM elements should be displayed or not. Displays DOM elements when first color is generated
 let appRan = false;
 const displayDOMOnStart = (appStarted) => {
   loading.classList.add("invisible");
@@ -32,7 +32,7 @@ const displayDOMOnStart = (appStarted) => {
   appRan = true;
 };
 
-//DOM Elements
+ // DOM Elements
 const loading = document.getElementById("loading");
 const RGBColorTitle = document.getElementById("RGBColorTitle");
 const hexColorTitle = document.getElementById("hexColorTitle");
@@ -46,7 +46,7 @@ const addToFavoritesButton = document.getElementById("addToFavoritesButton");
 const clearFavoritesButton = document.getElementById("clearFavoritesButton");
 const addCurrentColorButton = document.getElementById("addCurrentColorButton");
 
-//Function to obtain random number from 0-255 for RGB values
+ // Function to obtain random number from 0-255 for RGB values
 const getRandomNum = () => {
   return Math.round(Math.random() * 255);
 };
@@ -56,7 +56,7 @@ const changeColorTitlesText = (red, green, blue) => {
   hexColorTitle.textContent = `Hex: ${rgbToHex(red, green, blue)}`; 
 };
 
-//Function that converts RGB to hex
+ // Function that converts RGB to hex
 const rgbToHex = (red, green, blue) => {
   const convert = (color) => {
     let hex = Number(color).toString(16);
@@ -70,7 +70,7 @@ const rgbToHex = (red, green, blue) => {
 
 };
 
-//Function that obtains the color value of a clicked item in color list
+ // Function that obtains the color value of a clicked item in color list
 const colorItemOnClick = (e) => {
   if (prevItem !== null) {
     prevItem.classList.remove("highlightedItem");
@@ -88,20 +88,20 @@ const colorItemOnClick = (e) => {
   prevItem = e;
 };
 
-//Function that changes the background color (Only used for colorItemOnClick because repeat is set to false)
+ // Function that changes the background color (Only used for colorItemOnClick because repeat is set to false)
 const changeBgColor = (red, green, blue) => {
   document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
   rgbToHex(red, green, blue); 
 };
 
-//Function that ends main loop
+ // Function that ends main loop
 const endMainLoop = () => {
   if (repeat === true) {
     repeat = false;
   }
 };
 
-//Function that changes the color title to either black or white based on the current background color (Passed in color)
+ // Function that changes the color title to either black or white based on the current background color (Passed in color)
 const changeTitleColor = (red, green, blue) => {
   if (red * 0.299 + green * 0.587 + blue * 0.114 > 186) {
     RGBColorTitle.style.color = "#000000";
@@ -124,7 +124,7 @@ const foregroundTextColor = (red, green, blue) => {
   }
 };
 
-//Re-render functions for historyBox and favoritesBox
+ // Re-render functions for historyBox and favoritesBox
 const renderHistory = () => {
   historyBox.innerHTML = `<ul> ${historyArray.map((color, index) => `<li style="background-color: rgb(${color.red},${color.green},${color.blue}); color: ${foregroundTextColor(color.red, color.green, color.blue)};" data-red=${color.red} data-green=${color.green} data-blue=${color.blue} id='${index}' onclick=colorItemOnClick(this)>Red: ${color.red}, Green: ${color.green}, Blue: ${color.blue}`)} </ul>`;
 };
@@ -133,9 +133,9 @@ const renderFavorites = () => {
   favoritesBox.innerHTML = `<ul> ${favoritesArray.map((color, index) => `<li style="background-color: rgb(${color.red},${color.green},${color.blue}); color: ${foregroundTextColor(color.red, color.green, color.blue)};" data-red=${color.red} data-green=${color.green} data-blue=${color.blue} id='${index}' onclick=colorItemOnClick(this)>Red: ${color.red}, Green: ${color.green}, Blue: ${color.blue}`)} </ul>`;
 };
 
-//Event Listeners
+ // Event Listeners
 
-//For starting / stopping the color generation loop
+ // For starting / stopping the color generation loop
 startStopButton.addEventListener("click", () => {
   if (!repeat) {
     repeat = !repeat;
@@ -145,7 +145,7 @@ startStopButton.addEventListener("click", () => {
   repeat = !repeat;
 });
 
-//For clearing the history box and historyArray
+ // For clearing the history box and historyArray
 clearHistoryButton.addEventListener("click", () => {
   historyBox.innerHTML = "";
   historyArray = [];
@@ -153,7 +153,7 @@ clearHistoryButton.addEventListener("click", () => {
   currentItem = null;
 });
 
-//For adding the highlighted color from history to favorites
+ // For adding the highlighted color from history to favorites
 addToFavoritesButton.addEventListener("click", () => {
   if (currentItem !== null) {
     const targetArr = currentItem.textContent.split(",");
@@ -165,71 +165,71 @@ addToFavoritesButton.addEventListener("click", () => {
       green,
       blue
     });
-    //Map over all colors in the favorites array and display them in the favoritesBox
+     // Map over all colors in the favorites array and display them in the favoritesBox
     renderFavorites();
   }
 });
 
-//For clearing the favorites box and favoritesArray
+ // For clearing the favorites box and favoritesArray
 clearFavoritesButton.addEventListener("click", () => {
   favoritesBox.innerHTML = "";
   favoritesArray = [];
 });
 
-//For adding current background color to the favoritesArray
+ // For adding current background color to the favoritesArray
 addCurrentColorButton.addEventListener("click", () => {
   favoritesArray.push(currentColor);
   renderFavorites();
 });
 
-//Main loop that will loop over newly generated colors
+ // Main loop that will loop over newly generated colors
 const mainLoop = () => {
   setTimeout(() => {
-    //call displayDOMOnStart to display DOM elements
+     // call displayDOMOnStart to display DOM elements
     if (!appRan) {
       displayDOMOnStart(true);
     }
 
     if (repeat === true) {
-      //Generate new color values
+       // Generate new color values
       const red = getRandomNum();
       const green = getRandomNum();
       const blue = getRandomNum();
 
-      //Set background color based on generated values
+       // Set background color based on generated values
       changeBgColor(red, green, blue); 
 
-      //Change the RGBColorTitle with changeTitleColor function
+       // Change the RGBColorTitle with changeTitleColor function
       changeTitleColor(red, green, blue);
 
-      //Update the RGBColorTitle with the current background color
+       // Update the RGBColorTitle with the current background color
       changeColorTitlesText(red, green, blue);
 
-      //Push current color to the history array
+       // Push current color to the history array
       historyArray.push({
         red,
         green,
         blue
       });
 
-      //Update currentColor with the current color
+       // Update currentColor with the current color
       currentColor = {
         red,
         green,
         blue
       };
 
-      //Map over all colors in the history array and display them in the historyBox
+       // Map over all colors in the history array and display them in the historyBox
       renderHistory();
 
-      //Map over all colors in the favorites array and display them in the favoritesBox
+       // Map over all colors in the favorites array and display them in the favoritesBox
       renderFavorites();
 
-      //Call mainLoop for for looping
+       // Call mainLoop for for looping
       mainLoop();
     }
   }, 1500);
 };
 
-//Run mainLoop for the first time
+ // Run mainLoop for the first time
 mainLoop();
